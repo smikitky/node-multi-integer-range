3049ff77675d91ba5dd8bf8bab2be7e0e5de64de
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-07-14 15:26:10 +0900

    Export MultiRange also as the default export

--- multi-integer-range.ts
+++ multi-integer-range.ts
@@ -494,6 +494,8 @@ export class MultiRange {
 
 }
 
+export default MultiRange;
+
 // Set ES6 iterator, if Symbol.iterator is defined
 /* istanbul ignore else */
 if (typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol') {
