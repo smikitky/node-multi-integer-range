c1193c99487eb5ba52bc4b0ad8b4516d4fc47608
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-07-15 09:15:50 +0000

    Fix runkit exapmle (former tonic)

---
+++ example.js
@@ -0,0 +1,11 @@
+const { MultiRange, multirange } = require('multi-integer-range');
+
+// Just a nice shorthand for you
+const mr = value => multirange(value, { parseUnbounded: true, parseNegative: true });
+
+// Basic manipulation
+const range = mr('1-5, 9, 8').append(10).subtract(1);
+console.log(range.toString());
+
+// Iterator & array spread
+console.log([...mr('(-103)-(-100), 101-103')]);
